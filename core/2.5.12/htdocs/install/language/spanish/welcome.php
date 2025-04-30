920969eb6da18829ff417b2be9cb5313849eea63
Michael Beck <mambax7@gmail.com>
2025-04-30 04:04:07 -0400

    TinyMCE, updates

--- core/2.5.12/htdocs/install/language/spanish/welcome.php
+++ core/2.5.12/htdocs/install/language/spanish/welcome.php
@@ -17,7 +17,7 @@ $content = '
 <h3>Requisitos</h3>
 <ul>
     <li>Servidor WWW (<a href="https://www.apache.org/" rel="external">Apache</a>, <a href="https://www.nginx.com/" rel="external">NGINX</a>, IIS, etc.)</li>
-    <li><a href="https://www.php.net/" rel="external">PHP</a> 5.6.0 o superior, se recomienda 7.3 o superior</li>
+    <li><a href="https://www.php.net/" rel="external">PHP</a> 7.4 o superior, se recomienda 7.3 o superior</li>
     <li><a href="https://www.mysql.com/" rel="external">MySQL</a> 5.5 o superior, se recomienda 5.7 o superior</li>
 </ul>
 <h3>Antes de instalar</h3>
